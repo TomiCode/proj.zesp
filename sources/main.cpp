ad725cacb2a1f3103e7010eff298667b7fb841ae
Tomasz Król <tomicode@gmail.com>
2018-02-27 18:42:52 +0100

    Initial code commit.

---
+++ sources/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <memory>
+#include <map>
+#include <mutex>
+#include <atomic>
+#include <cstring>
+#include <string>
+#include <fstream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello world" << std::endl;
+	return 0;
+}
